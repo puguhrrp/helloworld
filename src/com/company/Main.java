8bc6221a2f570651063c44918166192362bc4ed2
Puguh Rohmanu Rizka Pratama <puguh.pratama@kemenkeu.go.id>
2018-07-27 10:03:44 +0700

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class Main{
+    public static void main(String[] args) {
+        int nilai = 1;
+        String pesan="", jumlah="";
+
+        if (nilai>2) {
+            jumlah = "Lebih besar dari 2";
+            if(nilai%2 == 0){
+                pesan = "Bilangan Genap";
+            }else{
+                pesan = "Bilangan Ganjil";
+            }
+            System.out.println(jumlah+" dan "+pesan);
+        }else{
+            jumlah = "Lebih kecil dari 2";
+            System.out.println(jumlah);
+        }
+    }
+
+}
